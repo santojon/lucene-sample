144ad8bf0baf6357e2f5c842c3edaa3eb960f0f3
Jonathan Santos <santojon5@gmail.com>
2016-04-16 15:56:34 +0000

    test

--- services/src/main/groovy/com/sample/lucene/service/IndexingService.groovy
+++ services/src/main/groovy/com/sample/lucene/service/IndexingService.groovy
@@ -1,12 +1,15 @@
 package com.sample.lucene.service
 
+import java.io.*
+import java.util.*
+import org.apache.commons.io.*
 import org.apache.lucene.analysis.*
 import org.apache.lucene.analysis.standard.*
 import org.apache.lucene.document.*
 import org.apache.lucene.index.*
+import org.apache.lucene.queryparser.*
 import org.apache.lucene.queryparser.classic.*
 import org.apache.lucene.queryparser.flexible.standard.*
-import org.apache.commons.io.FileUtils
 import org.apache.lucene.store.*
 import org.apache.lucene.search.*
 import org.apache.lucene.util.*
@@ -14,20 +17,24 @@ import org.apache.lucene.util.*
  
 class IndexingService {
     
-    public static final String FILES_TO_INDEX_DIRECTORY = "../data"
-	public static final String INDEX_DIRECTORY = "../index"
+    public static final String FILES_TO_INDEX_DIRECTORY = '../data'
+	public static final String INDEX_DIRECTORY = '../index'
 
-	public static final String FIELD_PATH = "path"
-	public static final String FIELD_CONTENTS = "contents"
+	public static final String FIELD_PATH = 'path'
+	public static final String FIELD_CONTENTS = 'contents'
     
     public static void createIndex() throws CorruptIndexException, LockObtainFailedException, IOException {
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40)
+		def inDir = new File(INDEX_DIRECTORY)
+		def indexDirectory = FSDirectory.open(inDir)
+		println 'dir --> ' indexDirectory
 		
-		boolean recreateIndexIfExists = true
-		IndexWriter indexWriter = new IndexWriter(INDEX_DIRECTORY, analyzer, recreateIndexIfExists)
+		def analyzer = new StandardAnalyzer(Version.LUCENE_40)
+		def writerConfiguration = new IndexWriterConfig(Version.LUCENE_40, analyzer)
+        def indexWriter = new IndexWriter(indexDirectory, writerConfiguration)
 		
 		File dir = new File(FILES_TO_INDEX_DIRECTORY)
 		File[] files = dir.listFiles()
+		println 'dir ' + files.length + ' --> ' files
 		
 		for (int i = 0; i < files.length; i++) {
 		    try {
@@ -37,8 +44,9 @@ class IndexingService {
     			String path = file.getCanonicalPath()
     			document.add(new TextField(FIELD_PATH, path, Field.Store.YES))
     
-    			Reader reader = new FileReader(file)
-    			document.add(new TextField(FIELD_CONTENTS, reader))
+    			String reader = FileUtils.readFileToString(file)
+    			document.add(new TextField(FIELD_CONTENTS, reader, Field.Store.YES))
+    			println 'dir ' + (i + 1) + ' --> ' document."$FIELD_PATH"
     
     			indexWriter.addDocument(document)
 		    } catch (Exception e) {
@@ -50,26 +58,30 @@ class IndexingService {
 		indexWriter.close()
 	}
 
-	public static void searchIndex(String searchString) throws IOException, ParseException {
-		System.out.println("Searching for '" + searchString + "'")
-		Directory directory = FSDirectory.getDirectory(INDEX_DIRECTORY)
-		IndexReader indexReader = IndexReader.open(directory)
-		IndexSearcher indexSearcher = new IndexSearcher(indexReader)
+	public static List searchIndex(String searchString) throws IOException, ParseException {
+	    List result = []
+		println("Searching for '" + searchString + "'")
+		
+		def directory = FSDirectory.open(INDEX_DIRECTORY)
+		def indexReader = IndexReader.open(directory)
+		def indexSearcher = new IndexSearcher(indexReader)
 
-		Analyzer analyzer = new StandardAnalyzer()
+		def analyzer = new StandardAnalyzer(Version.LUCENE_40)
 		QueryParser queryParser = new QueryParser(FIELD_CONTENTS, analyzer)
 		Query query = queryParser.parse(searchString)
 		ScoreDoc hits = indexSearcher.search(query)
-		System.out.println("Number of hits: " + hits.length())
+		println("Number of hits: " + hits.length())
 
 		Iterator<ScoreDoc> it = hits.iterator()
 		while (it.hasNext()) {
 			ScoreDoc hit = it.next()
 			Document document = hit.getDocument()
 			String path = document.get(FIELD_PATH)
-			System.out.println("Hit: " + path)
+			println("Hit: " + path)
+			result.add(document."$FIELD_CONTENTS")
 		}
 
+        return result
 	}
     
     List index() {
